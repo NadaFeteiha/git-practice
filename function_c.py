f9e5a7af88a68d601edd3b9c80061b68c15dd982
NadaFeteiha <nada.feteiha@gmail.com>
2026-09-22 08:56:14 -0700

    add function c to make conflict

--- function_c.py
+++ function_c.py
@@ -3,7 +3,10 @@ def merge_lists(list_a, list_b):
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    pass
+
+    #  try to do conflict. 
+
+    return list(set(list_a) | set(list_b))
 
 
 if __name__ == "__main__":
